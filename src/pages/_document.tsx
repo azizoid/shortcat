d25c788b89f2a2073b68f0df2aba98f3c915b108
azizoid <shaqhuseynov@gmail.com>
2023-03-27 16:17:05 +0200

    style: set basic styles

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="Shortcat.ly is a URL shortener service that creates short, easy-to-remember URLs that redirect to any website or resource on the internet. Simplify your online presence with Shortcat.ly." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
