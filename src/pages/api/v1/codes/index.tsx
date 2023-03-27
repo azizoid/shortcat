32418d8f63b2ce3e83eac5bb1fca8a88aeda917d
azizoid <shaqhuseynov@gmail.com>
2023-03-27 18:31:12 +0200

    feature: check if the method is Post and redirect_url field exists

--- src/pages/api/v1/codes/index.tsx
+++ src/pages/api/v1/codes/index.tsx
@@ -2,17 +2,28 @@ import { prismaHelper } from '@/utilities/prisma';
 import { Shortcat } from '@prisma/client';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-const { uuid } = require('uuidv4');
+import { v4 as uuidv4 } from 'uuid';
 
 const handler = async (
   req: NextApiRequest,
   res: NextApiResponse
 ) => {
+  if (req.method !== 'POST') {
+    throw new Error('Invalid request method.')
+  }
+
+  const { redirect_url } = req.body;
+
+  // TODO: add some other validaitons here
+  if (!redirect_url) {
+    throw new Error('Redirect Url is not provided.')
+  }
+
   try {
     const newShortcat = await prismaHelper<Shortcat>(async (prisma) => {
       const shortcatData = {
-        shortcode_guid: uuid(),
-        redurect_url: 'www.com',
+        shortcode_guid: uuidv4(),
+        redirect_url,
         active: true
       }
       return await prisma.shortcat.create({ data: shortcatData })
