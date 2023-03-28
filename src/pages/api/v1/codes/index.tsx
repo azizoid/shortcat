1a2c3ec3e618cb4ce13712c6df34179002364592
azizoid <shaqhuseynov@gmail.com>
2023-03-28 04:28:45 +0200

    feature: add get endpoint to `api/v1/codes` to return all rows

--- src/pages/api/v1/codes/index.tsx
+++ src/pages/api/v1/codes/index.tsx
@@ -4,36 +4,49 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 import { v4 as uuidv4 } from 'uuid';
 
-const handler = async (
-  req: NextApiRequest,
-  res: NextApiResponse
-) => {
-  if (req.method !== 'POST') {
-    throw new Error('Invalid request method.')
-  }
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  switch (req.method) {
+    case 'GET':
+      try {
+        const shortcat = await prismaHelper<Shortcat[]>((prisma) =>
+          prisma.shortcat.findMany()
+        );
 
-  const { redirect_url } = req.body;
+        if (!shortcat.length) {
+          return res.status(404).json({ message: 'Short URL not found' });
+        }
 
-  // TODO: add some other validaitons here
-  if (!redirect_url) {
-    throw new Error('Redirect Url is not provided.')
-  }
-
-  try {
-    const newShortcat = await prismaHelper<Shortcat>(async (prisma) => {
-      const shortcatData = {
-        shortcode_guid: uuidv4(),
-        redirect_url,
-        active: true
+        return res.status(200).json(shortcat);
+      } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Internal server error' });
       }
-      return await prisma.shortcat.create({ data: shortcatData })
-    });
 
-    res.status(200).json(newShortcat)
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Internal server error' });
+    case 'POST':
+      try {
+        const { redirect_url } = req.body;
+
+        // TODO: add some other validaitons here
+        if (!redirect_url) {
+          throw new Error('Redirect Url is not provided.')
+        }
+
+        const newShortcat = await prismaHelper<Shortcat>(async (prisma) => {
+          const shortcatData = {
+            shortcode_guid: uuidv4(),
+            redirect_url,
+            active: true
+          }
+          return await prisma.shortcat.create({ data: shortcatData })
+        });
+
+        res.status(200).json(newShortcat)
+      } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Internal server error' });
+      }
+
+    default:
+      return res.status(405).json({ message: 'Method not allowed' });
   }
 }
-
-export default handler
