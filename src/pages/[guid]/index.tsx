9fee66bc535ef8aa472ab8b032ad900738d5ec85
azizoid <shaqhuseynov@gmail.com>
2023-03-28 03:42:45 +0200

    feature: fetch data from api based on the guid

--- src/pages/[guid]/index.tsx
+++ src/pages/[guid]/index.tsx
@@ -1,3 +1,23 @@
-const GuidPage = () => <div>Guid Page</div>
+import { Shortcat } from "@prisma/client";
+import { useRouter } from "next/router"
+import useSWR from 'swr'
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+const GuidPage = () => {
+  const router = useRouter()
+  const { guid } = router.query
+
+  const { data, error } = useSWR<Shortcat>(guid ? `/api/v1/codes/${guid}` : null, fetcher)
+
+  if (error) return <div>Error fetching data</div>;
+  if (!data) return <div>Loading...</div>;
+
+  return <>
+    <div>GUID: {data.shortcode_guid}</div>
+    <div>Redirect Url: {data.redirect_url}</div>
+    <div>Active: {data.active}</div>
+  </>
+}
 
 export default GuidPage
