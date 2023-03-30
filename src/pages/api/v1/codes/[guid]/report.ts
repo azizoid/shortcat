2cebd2f4f12dba34a9e261e79036c17152a38514
azizoid <shaqhuseynov@gmail.com>
2023-03-30 06:23:03 +0200

    feature: report endpoint accepts GET and POST methods.

--- src/pages/api/v1/codes/[guid]/report.ts
+++ src/pages/api/v1/codes/[guid]/report.ts
@@ -1,13 +1,43 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { getClientIP } from "@/utilities/getClientIP/getClientIP";
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  url: string
-}
+const prisma = new PrismaClient();
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ url: 'codes/[guid]/report' })
+  try {
+    const {
+      query: { guid },
+      method,
+    } = req;
+
+    if (method === "GET") {
+      const visitCount = await prisma.report.count({
+        where: { shortcode_guid: String(guid) },
+      });
+
+      res.status(200).json({ visit_count: visitCount });
+    } else if (method === "POST") {
+      const ipAddress = getClientIP(req)
+
+      const visit = await prisma.report.create({
+        data: {
+          shortcode_guid: String(guid),
+          ip_address: ipAddress,
+        },
+      });
+
+      res.status(200).json(visit);
+    } else {
+      res.status(405).json({ message: "Method not allowed" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  } finally {
+    await prisma.$disconnect();
+  }
 }
