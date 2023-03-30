ccfe01f98f746c5c5f73e0c402e09e49baa776be
azizoid <shaqhuseynov@gmail.com>
2023-03-30 03:55:22 +0200

    style: put a cat icon after the logo because ShortCAT, update test snapshot

--- src/components/NavbarRow/NavbarRow.tsx
+++ src/components/NavbarRow/NavbarRow.tsx
@@ -1,4 +1,5 @@
 import { Button, Navbar } from 'flowbite-react'
+import { FaCat } from 'react-icons/fa';
 
 export const NavbarRow = () => <Navbar
   fluid={true}
@@ -8,6 +9,7 @@ export const NavbarRow = () => <Navbar
     <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
       Shortcat
     </span>
+    <FaCat />
   </Navbar.Brand>
   <div className="flex md:order-2">
     <Button href='/add'>
